edd18d7bae02c3abaaf82e9d9bb46c30e22d4a54
Michael Schout <mschout@gkg.net>
2010-03-30 16:15:07 +0000

    Import of MSCHOUT/App-Milter-Limit-Plugin-BerkeleyDB-0.51 from CPAN.
    gitpan-cpan-distribution: App-Milter-Limit-Plugin-BerkeleyDB
    gitpan-cpan-version:      0.51
    gitpan-cpan-path:         MSCHOUT/App-Milter-Limit-Plugin-BerkeleyDB-0.51.tar.gz
    gitpan-cpan-author:       MSCHOUT
    gitpan-cpan-maturity:     released

--- lib/App/Milter/Limit/Plugin/BerkeleyDB.pm
+++ lib/App/Milter/Limit/Plugin/BerkeleyDB.pm
@@ -1,5 +1,5 @@
 package App::Milter::Limit::Plugin::BerkeleyDB;
-$App::Milter::Limit::Plugin::BerkeleyDB::VERSION = '0.50';
+our $VERSION = '0.51';
 
 # ABSTRACT: BerkeleyDB driver for App::Milter::Limit
 
@@ -92,7 +92,7 @@ App::Milter::Limit::Plugin::BerkeleyDB - BerkeleyDB driver for App::Milter::Limi
 
 =head1 VERSION
 
-version 0.50
+version 0.51
 
 =head1 SYNOPSIS
 
